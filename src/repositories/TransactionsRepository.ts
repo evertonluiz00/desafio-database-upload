2f18ed0a79db0ad98773d4391918d91bb8a00901
Everton Christmann <evertonluiz00@hotmail.com>
2020-05-07 10:42:58 -0300

    Challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,11 +8,6 @@ interface Balance {
   total: number;
 }
 
-interface Resume {
-  transactions: Transaction[];
-  balance: Balance;
-}
-
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
